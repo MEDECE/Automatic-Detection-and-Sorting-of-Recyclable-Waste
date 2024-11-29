fae0716388105b210f960f48cfd40990d0e4d5c5
Meder <mederic.rolland@edu.ece.fr>
2024-11-29 15:15:47 +0900

    test

--- main.py
+++ main.py
@@ -14,3 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+yolo task=detect mode=predict model=runs/detect/train16/weights/best.pt source=C:/Users/Meder/PycharmProjects/PythonProject3/dataset/carton.jpg
+
